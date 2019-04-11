3be8b22667c171aef67eb1251d66575e3af99ae6
fenix3s <paulo.juniel@gmail.com>
2019-04-11 18:09:37 -0300

    adicionando variaves de ambiente

--- SemanaOminiStack_RocketSeat/backend/src/models/File.js
+++ SemanaOminiStack_RocketSeat/backend/src/models/File.js
@@ -18,7 +18,9 @@ const File = new mongoose.Schema(
 });
 
 File.virtual('url').get(funcion(){
-	return `http://localhost:3333/files/${encondeURIComponent(this.path)}`;
+	const url = process.env.URL || 'http://localhost:3333/'
+	
+	return `${url}/files/${encondeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model("File", File);
